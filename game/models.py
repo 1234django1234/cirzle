8f8742e90415a99c59755d410ee4026e89266271
Samer Salmeh <salmeh.samer@gmail.com>
2019-07-21 11:43:09 +0300

    add game models

--- game/models.py
+++ game/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class gmae(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='game_images/')
+    details = models.TextField(default='')
+    release_date = models.DateTimeField()
+    download_link = models.URLField(max_length=200)
