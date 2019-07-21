cc957b71728967b5fa3b5468c6c0d8c95775e48b
Samer Salmeh <salmeh.samer@gmail.com>
2019-07-21 13:41:43 +0300

    ~

--- cirzle/settings.py
+++ cirzle/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cirzledb',
         'USER': 'postgres',
-        'PASSWORD': '1234s1234S#',
+        'PASSWORD': 'django1234',
         'HOST':'localhost',
         'PORT': '5432'
     }
