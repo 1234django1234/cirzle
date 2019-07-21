22083ad687f92b330466462eea579427bf837eb2
Samer Salmeh <salmeh.samer@gmail.com>
2019-07-21 12:27:16 +0300

    create admin

--- game/models.py
+++ game/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class gmae(models.Model):
+class Game(models.Model):
     title = models.CharField(max_length=50)
     image = models.ImageField(upload_to='game_images/')
     details = models.TextField(default='')
