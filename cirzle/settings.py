b350f1d7ed3c67260bca01eab27af64ddf0aaafa
Samer Salmeh <salmeh.samer@gmail.com>
2019-07-21 12:49:10 +0300

    change database from db.sqlite3 to postgresql

--- cirzle/settings.py
+++ cirzle/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cirzle.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cirzledb',
+        'USER': 'postgres',
+        'PASSWORD': '1234s1234S#',
+        'HOST':'localhost',
+        'PORT': '5432'
     }
 }
 
